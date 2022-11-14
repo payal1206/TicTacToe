55941fd1ddd0c411b4a7ec96189dcf0388077b04
payal1206 <68314999+payal1206@users.noreply.github.com>
2022-11-14 18:36:06 +0530

    .

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/tt-t"));
+app.use(express.static("./dist/tt-t/index.html"));
 
 app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/tt-t/" }));
 
